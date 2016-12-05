507256dd41d7f01402cbf24060b9651415f0d125
Andrew Ray <me@andrewray.me>
2016-12-04 17:26:21 -0800

    Initial commit

---
+++ config/webpack.prod.js
@@ -0,0 +1,78 @@
+// Webpack config for development
+var fs = require('fs');
+var path = require('path');
+var webpack = require('webpack');
+var host = (process.env.HOST || 'localhost');
+var port = parseInt(process.env.PORT) + 1 || 3001;
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var babelrc = fs.readFileSync('./.babelrc');
+var babelrcObject = {};
+
+try {
+    babelrcObject = JSON.parse(babelrc);
+} catch (err) {
+    console.error('==>     ERROR: Error parsing your .babelrc.');
+    console.error(err);
+}
+
+var babelrcObjectDevelopment = babelrcObject.env && babelrcObject.env.development || {};
+
+// merge global and dev-only plugins
+var combinedPlugins = babelrcObject.plugins || [];
+combinedPlugins = combinedPlugins.concat(babelrcObjectDevelopment.plugins);
+
+var babelLoaderQuery = Object.assign({}, babelrcObjectDevelopment, babelrcObject, {plugins: combinedPlugins});
+delete babelLoaderQuery.env;
+
+// make sure react-transform is enabled
+babelLoaderQuery.plugins = babelLoaderQuery.plugins || [];
+
+module.exports = {
+    context: __dirname,
+    progress: true,
+    entry: {
+        'app-main': '../src/index.js'
+    },
+    output: {
+        path: 'build',
+        filename: '[name].js',
+        chunkFilename: '[name].js',
+        publicPath: 'https://andrewray.s3-us-west-2.amazonaws.com/',
+    },
+    module: {
+        loaders: [
+            { test: /\.jsx?$/, exclude: /node_modules|\.typeface\.js$/, loaders: ['babel?' + JSON.stringify(babelLoaderQuery)]},
+            { test: /\.scss$/, exclude: /global\.scss$/, loader: ExtractTextPlugin.extract('style', 'css?sourceMap&sourceMap!sass?outputStyle=expanded&sourceMap' ) },
+            { test: /\.png$/, loader: 'url-loader?limit=1024' },
+            { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
+            { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
+            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
+            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
+            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" },
+        ]
+    },
+    resolve: {
+        modulesDirectories: [
+            'src',
+            'node_modules'
+        ],
+        extensions: [ '', '.json', '.js', '.jsx' ]
+    },
+    plugins: [
+        new ExtractTextPlugin('[name]-[chunkhash].css', {allChunks: true}),
+        new webpack.NoErrorsPlugin(),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        }),
+        new HtmlWebpackPlugin({
+            template: '../src/index.html',
+            inject: 'body'
+        }),
+    ]
+};
---
+++ scripts/deploy.sh
@@ -0,0 +1,30 @@
+# Exit on any failure
+set -e
+
+COLOR_RED=$(tput sgr0 && tput setaf 1)
+COLOR_GREEN=$(tput sgr0 && tput setaf 2)
+COLOR_YELLOW=$(tput sgr0 && tput setaf 3)
+COLOR_DARK_BLUE=$(tput sgr0 && tput setaf 4)
+COLOR_BLUE=$(tput sgr0 && tput setaf 6)
+COLOR_PURPLE=$(tput sgr0 && tput setaf 5)
+COLOR_PINK=$(tput sgr0 && tput bold && tput setaf 5)
+COLOR_LIGHT_GREEN=$(tput sgr0 && tput bold && tput setaf 2)
+COLOR_LIGHT_RED=$(tput sgr0 && tput bold && tput setaf 1)
+COLOR_LIGHT_CYAN=$(tput sgr0 && tput bold && tput setaf 6)
+COLOR_RESET=$(tput sgr0)
+
+echo "${COLOR_LIGHT_CYAN}Syncing to git remote...${COLOR_RESET}"
+git push origin master
+
+echo "${COLOR_LIGHT_CYAN}Building assets...${COLOR_RESET}"
+rm -r build
+webpack --display-error-details --config config/webpack.prod.js
+
+echo "${COLOR_LIGHT_CYAN}Uploading static assets to server...${COLOR_RESET}"
+scp build/index.html aray:/var/www/andrewray.me/
+aws s3 cp build/ s3://andrewray/ --recursive
+
+echo "${COLOR_GREEN}Complete!${COLOR_RESET}"
+
+osascript -e 'display notification "Build and Push Completed" with title "Idiot"'
+say -v Hyst Push Completed
