4d321a027aa658099b86298ceabacab189f78ebe
Andrew Ray <me@andrewray.me>
2016-12-09 23:33:32 -0800

    subtle ao and mouse over

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import {
     MeshPhongMaterial, AmbientLight, Mesh, MultiMaterial, ObjectLoader,
     PerspectiveCamera, Scene, SphereGeometry, Vector3, WebGLRenderer,
-    PointLight, PointLightHelper, FaceNormalsHelper,
+    PointLight, PointLightHelper, FaceNormalsHelper, TextureLoader,
 } from 'three';
+import { easeOutExpo as ease, } from 'easing-utils';
 
 require('./bootstra-grid.scss');
 require('./main.scss');
@@ -12,11 +13,30 @@ const width = 438;
 
 const logoElement = document.querySelector( '.blog-title' );
 const wrapper = document.querySelector( '.title-link' );
+
 let opacityTween = 1;
 logoElement.style.opacity = opacityTween;
 
-const loader = new ObjectLoader();
-loader.load( require( './andrew-ray-3d-logo.json' ), obj => {
+const aoMapIntensity = 0.15;
+
+const objLoader = new ObjectLoader();
+const textureLoader = new TextureLoader();
+const spinTime = 1000;
+
+let spinning = false;
+
+Promise.all([
+    new Promise( ( resolve, reject ) =>
+        textureLoader.load( require( './ray-ambient.jpg' ), t => resolve( t ) )
+    ),
+    new Promise( ( resolve, reject ) =>
+        objLoader.load( require( './andrew-ray-3d-logo.json' ), t => resolve( t ) )
+    ),
+]).then( values => {
+    const aoMap = values[ 0 ];
+    const obj = values[ 1 ];
+    const spinStart = obj.children[ 1 ].rotation.z;
+    let spinTimeStart = 0;
     
     function animate() {
 
@@ -29,9 +49,33 @@ loader.load( require( './andrew-ray-3d-logo.json' ), obj => {
         obj.rotation.x = 0.1 * Math.sin( Date.now() * 0.001 );
         obj.rotation.y = 0.02 * Math.sin( ( 1 + Date.now() ) * 0.0005 );
 
+        if( spinning ) {
+
+            const { rotation } = scene.children[ 3 ].children[ 1 ];
+            const elapsed = ( Date.now() - spinTimeStart ) / spinTime;
+            rotation.z = spinStart + ( ease( elapsed ) * Math.PI * 2 );
+
+            if( elapsed >= 1 ) {
+                spinning = false;
+                rotation.z = spinStart;
+            }
+
+        }
+
         requestAnimationFrame( animate );
         renderer.render( scene, camera );
     }
+
+    function spinNow() {
+        if( !spinning ) {
+            spinTimeStart = Date.now();
+            spinning = true;
+        }
+    }
+
+    setInterval( spinNow, 10000 );
+
+    wrapper.addEventListener( 'mouseenter', spinNow );
 
     const container = document.createElement( 'div' );
     container.classList.add( 'render-wrap' );
@@ -69,11 +113,15 @@ loader.load( require( './andrew-ray-3d-logo.json' ), obj => {
 
     const mainMaterial = new MultiMaterial([
         new MeshPhongMaterial({
+            aoMap,
+            aoMapIntensity,
             shininess: 100,
             specular: 0xffffff,
             color: 0xFFFFFF,
         }),
         new MeshPhongMaterial({
+            aoMap,
+            aoMapIntensity,
             shininess: 100,
             specular: 0xffffff,
             color: 0x5C992E
@@ -82,11 +130,15 @@ loader.load( require( './andrew-ray-3d-logo.json' ), obj => {
 
     const aMaterial = new MultiMaterial([
         new MeshPhongMaterial({
+            aoMap,
+            aoMapIntensity,
             shininess: 100,
             specular: 0xffffff,
             color: 0xFF5933
         }),
         new MeshPhongMaterial({
+            aoMap,
+            aoMapIntensity,
             shininess: 100,
             specular: 0xffffff,
             color: 0xFFFFFF
@@ -97,6 +149,7 @@ loader.load( require( './andrew-ray-3d-logo.json' ), obj => {
         if( child instanceof Mesh ) {
             child.castShadow = true;
             child.receiveShadow = true;
+            //console.log( 'child.faceVertexUvs.length:', child.geometry.faceVertexUvs );
             child.geometry.faces.forEach( face => {
                 const { x, y, z, } = face.normal;
                 face.normal = new Vector3( x, -z, y, );
